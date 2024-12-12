deda30e9aaa0aa3532ad3e5cb85eb45ae9881544
PedroC55 <plbc@ua.pt>
2024-12-12 14:21:42 +0000

    Revert "Armoury Room base"
    This reverts commit 909b0baddc3cbb3352b0585104ae63b90449fb73.

--- cv_project/candle_light.gd
+++
@@ -1,5 +0,0 @@
-extends OmniLight3D
-
-func _process(delta):
-	var flicker_intensity = 0.9 + randf() * 0.2  # Random value between 0.9 and 1.1
-	light_energy = flicker_intensity  # Update the light's energy
